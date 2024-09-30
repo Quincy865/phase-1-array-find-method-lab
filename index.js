f6cd29ae2cbe4c6b306a4279b86614650a2b4729
Quincy Michael <michaelkirima865@gmail.com>
2024-09-30 15:29:06 +0300

    Array find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(records) {
+    const win = records.find(record => record.result === "W");
+    return win ? win.year : undefined;
+}
+
+const gameRecords = [
+    { year: "2015", result: "W" },
+    { year: "2016", result: "W" },
+    { year: "2017", result: "undefined"}
+];
+
+console.log(superbowlWin(gameWins));
